d345520fa7aa71e22faf229dd7787eddccad0c95
Jac <github@jmika.co.uk>
2021-05-24 18:51:15 +0100

    update phpliteadmin

---
+++ src/index.php
@@ -0,0 +1,21 @@
+<?php
+if ($handle = opendir('.')) 
+{
+    while (false !== ($entry = readdir($handle))) {
+        if ($entry != "." && $entry != "..") {
+            echo "$entry\n";
+        }
+    }
+    closedir($handle);
+}
+if ($handle = opendir('/db')) 
+{
+    while (false !== ($entry = readdir($handle))) {
+        if ($entry != "." && $entry != "..") {
+            echo "$entry\n";
+        }
+    }
+    closedir($handle);
+}
+include 'phpliteadmin.php';
+?>
