a2685c806997a046bb3eeb0a2af2ac91dd3a84ef
Jac <github@jmika.co.uk>
2021-05-24 21:28:26 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,12 +1,15 @@
 <?php
-echo "Welcome in phpLiteAdmin docker. \r\n";
-echo "<br>";
+echo "Welcome in phpLiteAdmin docker.";
 echo "List of the contetnt of the /db folder.\r\n";
 echo "<br>";
+foreach (glob("/db/*.db") as $filename) {
+    echo "$filename size " . filesize($filename) . "\n";
+    echo "<br>";
+}
 if ($handle = opendir('/db')) 
 {
     while (false !== ($entry = readdir($handle))) {
-        if ($entry != "." && $entry != ".." && $entry == "*.db") {
+        if ($entry != "." && $entry != ".." && $entry == ".db") {
             echo "$entry\r\n";
 			echo "<br>";
         }
