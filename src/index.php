36e6e4b81b25d0e1a3affaee4cbb51d0b7e54853
Jac <github@jmika.co.uk>
2021-05-24 19:04:09 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,18 +1,24 @@
 <?php
+echo "List of the contetnt of the local . folder.\r\n"
+echo "<br>";
 if ($handle = opendir('.')) 
 {
     while (false !== ($entry = readdir($handle))) {
         if ($entry != "." && $entry != "..") {
-            echo "$entry\n";
+            echo "$entry\r\n";
+			echo "<br>";
         }
     }
     closedir($handle);
 }
+echo "List of the contetnt of the /db folder.\r\n"
+echo "<br>";
 if ($handle = opendir('/db')) 
 {
     while (false !== ($entry = readdir($handle))) {
         if ($entry != "." && $entry != "..") {
-            echo "$entry\n";
+            echo "$entry\r\n";
+			echo "<br>";
         }
     }
     closedir($handle);
